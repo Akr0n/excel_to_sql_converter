8c97521ca0eda5ccf163a0e606545617a2a9033b
Akr0n <federico.bot@gmail.com>
2025-09-21 17:05:23 +0200

    RELEASE

--- excel_to_sql_converter.py
+++ excel_to_sql_converter.py
@@ -6,8 +6,7 @@ from tkinter import filedialog, messagebox
 from PIL import Image, ImageTk
 
 #versione corrente
-#__version__ = "2.1.9"
-APP_VERSION = "2.1.9"
+APP_VERSION = "1.0.11"
 
 logger = None
 
